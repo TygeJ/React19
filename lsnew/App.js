b60667425648e17746300965331910bd19fe47ef
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-01-21 16:06:57 -0700

    add button

---
+++ lsnew/App.js
@@ -0,0 +1,16 @@
+const App = () => (
+  <div>
+
+    const BasicButton = () => (
+      return <button>Click Me</button>;
+    );
+
+    const BasicInput = () => (
+      return <input type="text"
+    )
+
+
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
